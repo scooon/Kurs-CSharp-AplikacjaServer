90a6f7c563ed34de021eec22255585c85ac90d0c
Scooon <scooon@ymail.com>
2021-04-19 19:27:29 +0200

    19.04.2021 19:27

---
+++ WinSrv/Funkcje.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace WinSrv
+{
+    class Funkcje
+    {
+        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
+        public static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(
+        UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+        private static readonly UInt32 SPI_SETDESKWALLPAPER = 0x14;
+        private static readonly UInt32 SPIF_UPDATEINIFILE = 0x01;
+        private static readonly UInt32 SPIF_SENDWININICHANGE = 0x02;
+
+        public static int openCD()
+        {
+            return mciSendString("set cdaudio door open", null, 0, 0);
+        }
+
+        public static void fire(byte hour, byte minutes)
+        {
+            try
+            {
+                TimeSpan myTimeSpan1 = new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second);
+                TimeSpan myTimeSpan2 = new TimeSpan(hour, minutes, 00);
+                TimeSpan TsObj = new TimeSpan();
+                TsObj = myTimeSpan2.Subtract(myTimeSpan1);
+                Int32 milsec = Convert.ToInt32(TsObj.TotalMilliseconds);
+                if(milsec < 0)
+                {
+                    Console.WriteLine("Wydarzenie wywołane po czasie!");
+                }
+                else
+                {
+                    Timer My_Timer = new System.Threading.Timer(doBadThings, null, milsec, 0);
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Nie udało się uruchomić timera!");
+            }
+        }
+
+        public static void openWebsite(string target)
+        { 
+
+            try
+            {
+                System.Diagnostics.Process.Start(target);
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    Console.WriteLine(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                Console.WriteLine(other.Message);
+            }
+        }
+
+        public static void SetWallpaper(String path)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            key.SetValue(@"WallpaperStyle", 0.ToString()); // 2 is stretched
+            key.SetValue(@"TileWallpaper", 0.ToString());
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
+        static void doBadThings(object state)
+        {
+            string imgWallpaper = @"Wallpaper.jpg";
+            
+            if (File.Exists(imgWallpaper))
+            {
+                //SetWallpaper(imgWallpaper);
+            }
+
+            openCD();
+            openWebsite("http://www.microsoft.com");
+            Console.WriteLine("Fire!");
+        }
+    }
+}
--- WinSrv/Program.cs
+++ WinSrv/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+
+
+/*
+ * 
+ * tapeta pobieranie do program data
+ * dodawanie slowa
+ * kontrolki
+ * wieczny capslock
+ * wlaczanie capslocka
+ * obracanie
+ * przeniesienie paska narzedzi
+ * wyłączenie komputera
+ * wyciszanie dźwięku
+ * sprawdzanie czy okno nie zostało zamknięte
+ * 
+ */
 
 namespace WinSrv
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+            Funkcje.fire(18, 47);
             Server.listener = new System.Net.HttpListener();
             Server.listener.Prefixes.Add(Server.url);
             Server.listener.Start();
